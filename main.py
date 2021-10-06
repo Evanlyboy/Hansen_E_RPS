105c6c84c7ddb94761c6723b61c64ac2617b2620
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 17:35:59 -0400

    added python file

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Evan"
+eyeColor = "green/blue"
+heightInFeet = 5
+hairColour = "brown"
+
+print("My name is " + name + " and I have " + eyeColor + " eyes")
+print("I'm about " + str(heightInFeet) + " feet tall and have " + hairColour + " hair")
